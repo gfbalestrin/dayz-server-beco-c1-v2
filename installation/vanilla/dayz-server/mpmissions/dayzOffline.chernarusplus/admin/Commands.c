9d4955f1c7e3abede292926e7de98efc1ba93f5a
Guara <gfbalestrin@hotmail.com>
2025-11-08 15:27:42 -0300

    Updates

--- installation/vanilla/dayz-server/mpmissions/dayzOffline.chernarusplus/admin/Commands.c
+++ installation/vanilla/dayz-server/mpmissions/dayzOffline.chernarusplus/admin/Commands.c
@@ -1744,3 +1744,109 @@ void ProcessAttachmentsRecursive(EntityAI parentItem, array<ref ItemAttachmentDa
         }
     }
 }
+
+void ScanFences()
+{
+    array<Object> objects = new array<Object>;
+    GetGame().GetObjectsAtPosition(Vector(0,0,0), 99999, objects, NULL);
+
+    int count = 0;
+    string fencesJson = "";
+
+    foreach (Object obj : objects)
+    {
+        Fence fence = Fence.Cast(obj);
+        if (fence)
+        {
+            count++;
+            vector pos = fence.GetPosition();
+            vector ori = fence.GetOrientation();
+
+            string openState;
+            if (fence.IsOpened())
+                openState = "Aberto";
+            else
+                openState = "Fechado";
+
+            bool hasGate = fence.HasFullyConstructedGate();
+
+            string gateState;
+            if (hasGate)
+                gateState = "Sim";
+            else
+                gateState = "Não";
+
+            bool isLocked = fence.IsLocked();
+            string lockedState;
+            if (isLocked)
+                lockedState = "Sim";
+            else
+                lockedState = "Não";
+
+            // Coleta anexos (ex: camonet, arame farpado, cadeado)
+            TStringArray attachments = new TStringArray;
+            string attachmentsJson = "";
+            if (fence.GetInventory())
+            {
+                for (int i = 0; i < fence.GetInventory().AttachmentCount(); i++)
+                {
+                    EntityAI att = fence.GetInventory().GetAttachmentFromIndex(i);
+                    if (att)
+                    {
+                        string attType = att.GetType();
+                        attachments.Insert(attType);
+                        
+                        // Monta JSON de attachments
+                        if (attachmentsJson != "")
+                            attachmentsJson += ",";
+                        attachmentsJson += "\"" + attType + "\"";
+                    }
+                }
+            }
+
+            string attachmentList;
+            if (attachments.Count() > 0)
+                attachmentList = string.Join(", ", attachments);
+            else
+                attachmentList = "Nenhum";
+
+            string posStr = pos[0].ToString() + ", " + pos[1].ToString() + ", " + pos[2].ToString();
+            string oriStr = ori[0].ToString() + ", " + ori[1].ToString() + ", " + ori[2].ToString();
+            string logMsg = "[FENCE] Posição=(" + posStr + ") | Ori=(" + oriStr + ") | Portão: " + gateState + " | Estado: " + openState + " | Trancado: " + lockedState + " | Anexos: " + attachmentList;
+            Print(logMsg);
+            WriteToLog(logMsg, LogFile.INIT, false, LogType.INFO);
+
+            // Monta JSON do fence
+            if (fencesJson != "")
+                fencesJson += ",";
+            string posX = pos[0].ToString();
+            string posZ = pos[1].ToString();
+            string posY = pos[2].ToString();
+            string oriX = ori[0].ToString();
+            string oriY = ori[1].ToString();
+            string oriZ = ori[2].ToString();
+            string hasGateStr = hasGate.ToString();
+            string isOpenedStr = fence.IsOpened().ToString();
+            string isLockedStr = isLocked.ToString();
+
+            fencesJson += "{\"position\":{\"x\":" + posX + ",\"z\":" + posZ + ",\"y\":" + posY + "}";
+            fencesJson += ",\"orientation\":{\"x\":" + oriX + ",\"y\":" + oriY + ",\"z\":" + oriZ + "}";
+            fencesJson += ",\"has_gate\":" + hasGateStr;
+            fencesJson += ",\"is_opened\":" + isOpenedStr;
+            fencesJson += ",\"is_locked\":" + isLockedStr;
+            fencesJson += ",\"attachments\":[" + attachmentsJson + "]}";
+        }
+    }
+
+    // Envia JSON via ExternalAction
+    if (fencesJson != "")
+    {
+        string jsonAction = "{\"action\":\"fences_positions\",\"fence_data\":[" + fencesJson + "]}";
+        AppendExternalAction(jsonAction);
+        WriteToLog("ScanFences(): JSON com " + count.ToString() + " fences enviado via ExternalAction", LogFile.INIT, false, LogType.INFO);
+    }
+
+    string summary = "[FENCE SCAN] Total de estruturas (Fence) encontradas: " + count.ToString();
+    Print(summary);
+    WriteToLog(summary, LogFile.INIT, false, LogType.INFO);
+}
