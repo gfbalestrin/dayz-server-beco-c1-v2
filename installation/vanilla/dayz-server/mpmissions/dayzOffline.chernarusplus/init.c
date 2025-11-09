4eefe71d8855783dd0f4110e3dfd9448a9c2cb0b
Guara <gfbalestrin@hotmail.com>
2025-11-09 17:57:26 -0300

    Updates

--- installation/vanilla/dayz-server/mpmissions/dayzOffline.chernarusplus/init.c
+++ installation/vanilla/dayz-server/mpmissions/dayzOffline.chernarusplus/init.c
@@ -285,8 +285,7 @@ class CustomMission: MissionServer
 		InitializeVestGrenadeSlots();
 
 		ActivePlayers = new array<ref ActivePlayer>();
-		InitWorldTracking();
-		//GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(InitWorldTracking, 5000, false);
+		GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(InitWorldTracking, 5000, false);
 		//if (!IsDeathmatchEnabled)
 		//{
 			// Loop contínuo para aplicar efeitos aos admins
