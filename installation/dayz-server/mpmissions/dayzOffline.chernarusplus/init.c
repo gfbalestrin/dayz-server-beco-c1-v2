0e5e70cab6a28d17ae0ae2aeac67fb2d4f2e83c6
Guara <gfbalestrin@hotmail.com>
2025-12-28 20:03:39 -0300

    Updates

--- installation/dayz-server/mpmissions/dayzOffline.chernarusplus/init.c
+++ installation/dayz-server/mpmissions/dayzOffline.chernarusplus/init.c
@@ -204,6 +204,10 @@ class CustomMission: MissionServer
 	override void OnUpdate(float timeslice)
 	{
 		super.OnUpdate(timeslice);
+		if (!IsDeathmatchEnabled && !MonitorMode)
+		{
+			return;
+		}
 		m_AdminCheckTimer10 += timeslice;
 		m_AdminCheckTimer60 += timeslice;
 		m_AdminCheckTimer600 += timeslice;
