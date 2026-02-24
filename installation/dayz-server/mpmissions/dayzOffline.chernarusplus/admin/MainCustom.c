250ac0a7aae2c04828eececdae86dbd5d40508fa
Guara <gfbalestrin@hotmail.com>
2026-02-24 13:23:26 -0300

    Updates

--- installation/dayz-server/mpmissions/dayzOffline.chernarusplus/admin/MainCustom.c
+++ installation/dayz-server/mpmissions/dayzOffline.chernarusplus/admin/MainCustom.c
@@ -33,35 +33,12 @@ void MainCustom()
 				}
 			}
 		}
-	} else {
-		// Força o horário para 06:00
-		hour = 10;
-		minute = 0;
+	} else {	
 
-		if ((month == reset_month) && (day < reset_day))
-		{
-			WriteToLog("main(): Ajustando data para " + reset_month + "/" + reset_day, LogFile.INIT, false, LogType.INFO);
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else if ((month == reset_month + 1) && (day > reset_day))
-		{
-			WriteToLog("main(): Ajustando data para " + reset_month + "/" + reset_day, LogFile.INIT, false, LogType.INFO);
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else if ((month < reset_month) || (month > reset_month + 1))
-		{
-			WriteToLog("main(): Ajustando data para " + reset_month + "/" + reset_day, LogFile.INIT, false, LogType.INFO);
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else
-		{
-			// Mesmo se não for necessário ajustar a data, ainda força o horário para 06
-			GetGame().GetWorld().SetDate(year, month, day, hour, minute);
-			WriteToLog("main(): Data mantida, horário ajustado para 06:00.", LogFile.INIT, false, LogType.INFO);
-		}
+		GetGame().GetWorld().SetDate(year, 9, 22, 10, 0);
 
 		// >>> Clima CLEAR no start
-		//SetClearWeatherNow();
+		SetClearWeatherNow();
 
 		// (Opcional) Reaplica após alguns segundos, caso algum subsistema mude o clima muito cedo
 		//GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(SetClearWeatherNow, 5000, false);
@@ -70,21 +47,21 @@ void MainCustom()
 
 void SetClearWeatherNow()
 {
+	
     Weather weather = GetGame().GetWeather();
     if (!weather) return;
 
     weather.MissionWeather(true);
 
-	int yy, mm, dd, hh, mins;
-    GetGame().GetWorld().GetDate(yy, mm, dd, hh, mins);
-	GetGame().GetWorld().SetDate(yy, 9, 22, 10, 0);
-
+	weather.GetOvercast().Set(1.0, 1, 0);
+                
+	// Mas forçamos a chuva e a neblina volumétrica a ficarem no zero absoluto
 	weather.GetRain().Set(0.0, 1, 0);
-	weather.GetOvercast().Set(0.01, 1, 0);
 	weather.GetFog().Set(0.0, 1, 0);
+	
 	weather.SetWindSpeed(0.0);
 	weather.SetWindMaximumSpeed(0.0);
-	weather.SetWindFunctionParams(0, 0, 0);
+	
 }
 
 void SetClearWeatherNowOld()
